95ecffc31f90b19800aa72f523cc26197110bfdb
kolob2000 <49161276+kolob2000@users.noreply.github.com>
2021-09-29 00:07:26 +0300

    add second task

--- tasks.sql
+++ tasks.sql
@@ -1,11 +1,14 @@
 -- 1.
 
 -- 2.
+USE shop;
+SELECT name AS product_item, (SELECT name FROM catalogs WHERE id = products.catalog_id) AS catalog_item
+FROM products;
 
 -- 3.
 USE aeroflot;
 
 SELECT id,
        (SELECT name FROM cities WHERE label = flights.from) AS 'from',
-       (SELECT name FROM cities WHERE label = flights.to)                  AS 'to'
+       (SELECT name FROM cities WHERE label = flights.to)   AS 'to'
 FROM flights;
