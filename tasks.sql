855318183c8fcdf45147adf995095d083b4876d4
kolob2000 <49161276+kolob2000@users.noreply.github.com>
2021-10-03 21:03:46 +0300

    add first task

--- tasks.sql
+++ tasks.sql
@@ -2,7 +2,7 @@ USE vk;
 
 -- 1.
 
-SELECT count(to_user_id), m.to_user_id, m.from_user_id FROM messages m
+SELECT m.from_user_id sender, m.to_user_id recipient, count(to_user_id) message_quantity  FROM messages m
 join users u
 ON m.from_user_id = u.id AND u.id = 34;
 
